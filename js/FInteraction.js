8b9f41edd365637f33c76e349ada5e5f81c84b00
Gm <contact@eximstudio.com>
2021-04-02 22:36:08 +0530

    FInteraction methods

--- js/FInteraction.js
+++ js/FInteraction.js
@@ -3,10 +3,11 @@ class FInteraction {
      * 
      * @param {*} client 
      * @param {{}} res 
-     * @param {{channel:object, user:object, guild:object, message:object}} extras 
+     * @param {{channel:object, user:object, guild:object, message:object, interaction:object}} extras 
      */
     constructor(client, res, extras){
         this.id = res.id
+        this.interaction = extras.interaction
         this.type = res.type
         this.client = client
         this.content = res.content
@@ -25,5 +26,72 @@ class FInteraction {
         this.flags = res.flags
         this.webhookID = res.webhook_id
         this.message = extras.message        
-    }    
+    }
+     /**
+     * 
+     * @param {*} res - the MessageEmbed object or string
+     */
+      send(res){
+        if (res) {
+            if (typeof res == 'string') {
+            this.client.api.webhooks(this.client.user.id, this.interaction.token).post({
+                data:{
+                    type:3,
+                    data:{
+                        content: res
+                    }
+                }
+            })
+        }else if (typeof res == 'object'){
+            this.client.api.webhooks(this.client.user.id, this.interaction.token).post({
+                data:{
+                    type:3,
+                    data: createAPIMessage(this.interaction, res, this.client)
+                }
+            })
+        }else {
+            throw new Error('INVALID Response type response should be a messageembed object or a string')
+        }
+    }
+    }
+    /**
+     * 
+     * @param {*} res - the MessageEmbed object or string
+     */
+    reply(res){
+        if (res) {
+            if (typeof res == 'string') {
+            this.client.api.webhooks(this.client.user.id, this.interaction.token).post({
+                data:{
+                    type:4,
+                    data:{
+                        content: res
+                    }
+                }
+            })
+        }else if (typeof res == 'object'){
+            this.client.api.webhooks(this.client.user.id, this.interaction.token).post({
+                data:{
+                    type:4,
+                    data: createAPIMessage(this.interaction, res, this.client)
+                }
+            })
+        }else {
+            throw new Error('INVALID Response type response should be a messageembed object or a string')
+        }
+    }
+    }
+    /**
+     * 
+     * @param {*} content - the MessageEmbed object or string
+     */
+    edit(content){
+        if (!content) throw new Error('content can\'t be empty')
+        const {data} = APIMessage.create(this.client.channels.resolve(this.channel.id), content)
+        return this.client.api.webhooks(this.client.user.id, this.token).messages(this.message.id).patch({ data })
+    }
+
+    delete(){
+        this.client.api.webhooks(this.client.user.id, this.token).messages(this.message.id).delete()
+    }
 }
