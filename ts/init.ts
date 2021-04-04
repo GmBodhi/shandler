41349976fb99db4c4b8a8914df2ef8b1ca646901
Gm <contact@eximstudio.com>
2021-04-04 12:56:56 +0530

    minor

--- ts/init.ts
+++ ts/init.ts
@@ -1,19 +1,16 @@
-const {createAPIMessage} = require('./api')
-const {Interaction} = require('./interaction')
-const {APIMessage} = require('discord.js')
-/**
- * 
- * @param {Object} interaction - Interaction object from the discord api
- * @param {Object} client - Discord.js client object
- */
-module.exports = async (interaction, client) => {
-    const member = await client.guilds.fetch(interaction.guild_id).then(g => g.members.fetch(interaction.member.user.id))
+import createAPIMessage from './api'
+import Interaction from './interaction'
+import {APIMessage} from 'discord.js'
+
+export default async (interaction:any, client:any) => {
+    const member = await client.guilds.fetch(interaction.guild_id).then((g:any) => g.members.fetch(interaction.member.user.id))
     const guild = await client.guilds.fetch(interaction.guild_id)
     const channel = await client.channels.fetch(interaction.channel_id)
     const Options = {
         guild,
         channel,
-        member
+        member,
+        client
     }
     interaction = new Interaction(interaction, Options)
     const { options } = interaction.data
