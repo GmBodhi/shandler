a11d2e6c0b1f5af1edf7b092a6ec36548dc1feb6
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-06-08 08:51:42 +0530

    minor

--- js/ButtonClickCollector.js
+++
@@ -1,8 +0,0 @@
-const { Collector } = require('discord.js')
-
-class ButtonClickCollector extends Collector {
-    constructor(int, filter, options = {}){
-        super(int.client, filter, options)
-        
-    }
-}
