ce2ddf74621c7ebb3e70fce4d6b53b0fa4cabc6f
Leonardo Bagi <leonardobagi123@gmail.com>
2022-02-02 20:54:32 -0400

    modified:   src/builders/option.ts 	modified:   src/builders/slashBuilder.ts

--- src/builders/slashBuilder.ts
+++ src/builders/slashBuilder.ts
@@ -1,47 +1,79 @@
-import { APIApplicationCommand } from "discord-api-types";
+import { APIApplicationCommand, Snowflake } from "discord-api-types";
+import { ApplicationCommandPermissionsManager } from "discord.js";
 
 import { SlashCommandOption } from "./option";
 
 let externalData: Partial<APIApplicationCommand> = {
-  type: 1,
-  options: [],
+    type: 1,
+    options: [],
 };
 
 export const setExternalData = (
-  fn: (
-    state: Partial<APIApplicationCommand>
-  ) => Partial<APIApplicationCommand> | Partial<APIApplicationCommand>
+    fn: (state: Partial<APIApplicationCommand>) => Partial<APIApplicationCommand> | Partial<APIApplicationCommand>
 ) => {
-  if (typeof fn === "object") Object.assign(externalData, fn);
-  else externalData = fn(externalData);
+    if (typeof fn === "object") Object.assign(externalData, fn);
+    else externalData = fn(externalData);
 };
 
+interface ShandlerGuildCommandPermissionOptions {
+    /** Users allowed to use a command */
+    users?: Snowflake | Snowflake[];
+    /** Roles allowed to use a command */
+    roles?: Snowflake | Snowflake[];
+    /** Whether this command should be available to moderators.
+     *
+     * Defaults to true
+     */
+    mods?: true | boolean;
+}
+
+interface ShandlerGuildCommandPermissions {
+    id: string;
+    permissions?: ShandlerGuildCommandPermissionOptions;
+}
+
+interface ShandlerCommandOptions {
+    name: string;
+    description: string;
+    guilds?: ShandlerGuildCommandPermissions[] | Snowflake[];
+    options?: SlashCommandOption[];
+
+    /** Don't set this param. It's meant for the module and it's just useless set it */
+    application_id?: string,
+    /** Don't set this param. It's meant for the module and it's just useless to set it*/
+    guild_id?: string
+}
+
 export class SlashBuilder {
-  data: APIApplicationCommand;
-  constructor(data: APIApplicationCommand) {
-    this.data = data ?? externalData;
-    this.data.application_id = externalData.application_id!;
-  }
+    constructor(public data: ShandlerCommandOptions) {
+        this.data = data ?? externalData;
+        this.data.application_id = externalData.application_id!;
+    }
 
-  setName(name: string) {
-    this.data.name = name;
-    return this;
-  }
+    setName(name: string) {
+        this.data.name = name;
+        return this;
+    }
 
-  setDescription(description: string) {
-    this.data.description = description;
-    return this;
-  }
+    setDescription(description: string) {
+        this.data.description = description;
+        return this;
+    }
 
-  setGuild(guildId: string) {
-    this.data.guild_id = guildId;
-  }
+    setGuild(guildId: string) {
+        this.data.guild_id = guildId;
+        return this;
+    }
 
-  addOption(option: SlashCommandOption) {
-    this.addOptions(option);
-  }
+    addOption(option: SlashCommandOption) {
+        this.addOptions(option);
+        return this;
+    }
 
-  addOptions(...options: SlashCommandOption[]) {
-    this.data.options?.push(...options.map((o) => o.toJSON()));
-  }
+    addOptions(...options: SlashCommandOption[]) {
+        options.forEach(option => {
+          this.data.options?.push(option)
+        })
+        return this;
+    }
 }
